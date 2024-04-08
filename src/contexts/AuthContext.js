34847c789159efcb0c36f76016e2a35692890ccf
Mo Chahadeh <applednd2002@gmail.com>
2024-04-09 00:54:51 +0300

    Fixed Double socket connection issue, successfuly established one socket connection!!!

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -3,6 +3,37 @@ import { io } from "socket.io-client";
 
 export const AuthContext = createContext();
 
+let useEffectCalled = false;
+let socketCreated = false;
+
+const initSocket = (token) => {
+
+    const socket = io(process.env.REACT_APP_ROOT_URL, {
+        auth: {
+            token
+        }
+    }, {
+        withCredentials: true
+    });
+
+    socket.on("connect", () => {
+        console.log("Connected to socket!");
+    });
+
+    socket.on("disconnect", () => {
+        console.log("Disconnected from socket!");    
+    });
+
+    socket.on("message", (data) => {
+
+        console.log("Received message: ", data);
+
+    })
+
+    return socket
+
+}
+
 const setAuth = (state, action) => {
 
     switch (action.type) {
@@ -11,14 +42,13 @@ const setAuth = (state, action) => {
             localStorage.setItem("user", JSON.stringify(action.payload.user));
             localStorage.setItem("loggedIn", true)
 
-            if(!state.socket) {
-                const socket = io(process.env.REACT_APP_ROOT_URL, {
-                    auth: {
-                        token: action.payload.token
-                    }
-                }, () => {
-                    console.log("Connected to socket");
-                });
+            if(!socketCreated) {
+                console.log("No socket, creating new one");
+                
+                const socket = initSocket(action.payload.token);
+
+                if(socket) socketCreated = true;
+
                 return {
                     loggedIn: true,
                     token: action.payload.token,
@@ -55,7 +85,6 @@ const setAuth = (state, action) => {
     }
 }
 
-
 export const AuthProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(setAuth, {
@@ -68,18 +97,34 @@ export const AuthProvider = ({ children }) => {
     // Runs once at startup to check if there is already a token saved in localStorage
     useEffect(() => {
 
-        const token = localStorage.getItem("token");
-        if (token) {
-            const user = JSON.parse(localStorage.getItem("user"));
-            if(!user) return;
-            dispatch({
-                type: "LOGIN",
-                payload: {
-                    token,
-                    user
-                }
-            });
+        if(useEffectCalled) return;
+
+        useEffectCalled = true;
+
+        const unmountFunc = () => {
+            if(state.socket) {
+                state.socket.disconnect();
+            }
         }
+
+        if(state.token) return unmountFunc;
+
+        const user = JSON.parse(localStorage.getItem("user"));
+        const token = localStorage.getItem("token");
+
+        if( !user || !token ) return unmountFunc;
+
+        console.log("Logging in with token from local storage");
+        dispatch({
+            type: "LOGIN",
+            payload: {
+                user,
+                token
+            }
+        });
+
+        return unmountFunc;
+        
     }, [])
 
     return (
